31c5d77b0fb071c109fbc3b13d7b2529f426f759
BrooitsFeiskJR <luizantonio0125@gmail.com>
2023-04-03 15:59:44 -0300

    Adding The Exercise Screen - Intents and Customizing our Toolbar

---
+++ app/src/main/java/dev/tontech/workoutapp/ExerciseActivity.kt
@@ -0,0 +1,28 @@
+package dev.tontech.workoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.tontech.workoutapp.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExerciseBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if(supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
