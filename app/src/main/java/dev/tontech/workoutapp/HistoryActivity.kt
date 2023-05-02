4e6b1d19eb920c116709f32952ff53ef59980e05
BrooitsFeiskJR <luizantonio0125@gmail.com>
2023-05-02 17:50:03 -0300

    Fetching Data from our Database and displaying it in the log

--- app/src/main/java/dev/tontech/workoutapp/HistoryActivity.kt
+++ app/src/main/java/dev/tontech/workoutapp/HistoryActivity.kt
@@ -2,7 +2,11 @@ package dev.tontech.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import dev.tontech.workoutapp.databinding.ActivityHistoryctivityBinding
+import dev.tontech.workoutapp.model.HistoryDao
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
     private var binding: ActivityHistoryctivityBinding? = null
@@ -21,6 +25,19 @@ class HistoryActivity : AppCompatActivity() {
 
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        val dao = (application as WorkoutApplication).db.HistoryDao()
+        getAllCompletedDates(dao)
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao) {
+        lifecycleScope.launch {
+            historyDao.fetAllDates().collect {allCompletedDates ->
+                for (i in allCompletedDates) {
+                    Log.e("Date: ", ""+i)
+                }
+            }
         }
     }
 
--- app/src/main/java/dev/tontech/workoutapp/model/HistoryDao.kt
+++ app/src/main/java/dev/tontech/workoutapp/model/HistoryDao.kt
@@ -2,11 +2,14 @@ package dev.tontech.workoutapp.model
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface HistoryDao {
     @Insert
     suspend fun insert(history: History)
 
-
+    @Query("SELECT * FROM `history-table`")
+    fun fetAllDates(): Flow<List<History>>
 }
